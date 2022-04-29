9b09b2d4802c54a08e57f81658dbd9718c9d7258
sharkbyte <sgquin@gmail.com>
2022-04-29 14:25:19 -0700

    finished post route

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const express= require ('express');
-const database =require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
 
-database.createConnection(
-    {
-        host: "localhost",
-        user:"root",
-        password:"Ak_9586!",
-        database:"movie_db"
-    },
-    console.log("coneccted to movie db")
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "movie_db",
+  },
+  console.log("coneccted to movie db")
 );
+
+app.get("/api/movies", (req, res) => {
+  db.query("SELECT * FROM movies", (err, result) => {
+    err ? console.log(err) : res.json(result);
+  });
+});
+
+app.post("/api/add-movie", (req, res) => {
+  const movieTitle = req.body.movieTitle;
+  db.query(
+    `INSERT INTO movies (movie_name) 
+    VALUES (?)`,
+    movieTitle,
+    (err, result) => {
+      if (err) {
+        res.status(500);
+      } else {
+        res.send(`${movieTitle} added to db`);
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log("server running ");
+});
