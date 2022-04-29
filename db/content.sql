1f0502d9df3fc41a6b6da189ea8767d3855e841d
akaur091 <amniderkaur.09@gmail.com>
2022-04-29 13:32:19 -0700

    add db

---
+++ db/content.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movie_name)
+VALUES
+    ("The Lion King"),
+    ("The Matrix"),
+    ("Twilight"),
+    ("The Gaurdians of the Galaxy"),
+    ("Candyman");
+
+INSERT INTO reviews (id, movie_id, review)
+VALUES
+    (1,1,"this movie is good"),
+    (2,2,"this movie is epic"),
+    (3,2,"I hated this movie");
